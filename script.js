5fd6509c24d7daa0129706f215185ebeca4b64fd
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-19 16:50:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+     const prices=document.querySelectorAll(".price");
+	let totalsumOfPrice=0;
+	for(let price of prices){
+		let priceRs=parseInt(price.textContent)
+		totalsumOfPrice+=priceRs;
+	}
+	let row=document.createElement("tr");
+	row.innerHTML=`<td>Total Price</td><td>${totalsumOfPrice}</td>`
+	const table=document.querySelector("table");
+	table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
