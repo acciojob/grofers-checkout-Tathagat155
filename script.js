571fc447b3d23fd1a308584ea60b0833eac29df8
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-19 17:28:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const getSum = () => {
 	}
 	let row=document.createElement("tr");
 	 row.classList.add("total-price-row"); // Important!
-	row.innerHTML=`<td>Total Price  ${totalsumOfPrice}</td>`
+	row.innerHTML=`<td>${totalsumOfPrice}</td>`
 	const table=document.querySelector("table");
 	table.appendChild(row);
 };
